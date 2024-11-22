24de20cf9caf480e000cc20441ba7b854baa399c
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-11-23 02:30:29 +0400

    update BusinessController

--- src/main/java/com/example/kegichivka/controller/BusinessController.java
+++ src/main/java/com/example/kegichivka/controller/BusinessController.java
@@ -1,7 +1,11 @@
 package com.example.kegichivka.controller;
 
+import com.example.kegichivka.exception.ResourceNotFoundException;
 import com.example.kegichivka.model.BusinessUser;
+import com.example.kegichivka.service.BusinessUserService;
+import com.example.kegichivka.service.UserPrincipal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/business")
 @RequiredArgsConstructor
 public class BusinessController {
+    private final BusinessUserService businessUserService;
+
 
     @GetMapping("/dashboard")
-    public String dashboard(Model model, @AuthenticationPrincipal BusinessUser user) {
-        model.addAttribute("user", user);
+    public String dashboard(Model model, Authentication authentication) {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        BusinessUser businessUser = businessUserService.getBusinessUserByEmail(userPrincipal.getEmail())
+                .orElseThrow(() -> new ResourceNotFoundException("BusinessUser not found"));
+        model.addAttribute("user", businessUser);
         return "business/dashboard";
     }
 }
